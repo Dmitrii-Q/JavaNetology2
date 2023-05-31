745742a2f1693716b9fdf27d5ce06b9e59352659
Dmitrii-Q <120948230+Dmitrii-Q@users.noreply.github.com>
2023-05-31 18:14:43 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,7 @@
 public class Main {
-  public static void main(String[] args) {
-  
-    // Объявляете переменные для входных данных и
-    // параметров программы: одну для хранения 
-    // стоимости билета, другую для хранения количества
-    // рублей для одной бонусной мили
-    
-    // Рассчитываете количество бонусных миль, используя
-    // значения заведённых переменных. Ответ сохраняете в
-    // новую переменную и выводите на экран
-  }
-}
-
-public class Main {
     public static void main(String[] args) {
         int price = 5_000;
         int bonusMili = price / 20;
         System.out.println("Бонус Мили - " + bonusMili);
     }
 }
-Бонус Мили - 250
-
-Process finished with exit code 0
